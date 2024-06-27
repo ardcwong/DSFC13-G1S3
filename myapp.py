ca521424070e8d70d7d400335d0d0c8902ca6f73
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-06-28 03:12:33 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -57,13 +57,13 @@ if my_page == 'About MedInfoHub':
         initializing()
         if status =='ready':
             tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
-                data = np.random.randn(10, 1)
-                
-                tab1.subheader("A tab with a chart")
-                tab1.line_chart(data)
-                
-                tab2.subheader("A tab with the data")
-                tab2.write(data)
+            data = np.random.randn(10, 1)
+            
+            tab1.subheader("A tab with a chart")
+            tab1.line_chart(data)
+            
+            tab2.subheader("A tab with the data")
+            tab2.write(data)
 
    
 
