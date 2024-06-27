b280b44e778f5b3957d56e77121d574d440e5f99
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-06-28 01:59:09 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -30,7 +30,7 @@ my_page = st.sidebar.radio('Page Navigation', ['About MedInfoHub','Keyword', 'To
 df = pd.read_csv('data/medquad.csv')
 df = df.iloc[:2000]
 if my_page == 'About MedInfoHub':
-    st.image('banner.png')
+    st.image('data/banner.png')
     st.write("MedInfoHub is a comprehensive healthcare app designed to provide accessible medical information to patients and healthcare providers.Leveraging the power of the MedQuAD dataset* and advanced AI, MedInfoHub offers reliable answers to medical questions, supports telemedicine consultations, and enhances public health literacy. Whether you’re a patient seeking to understand your health better or a healthcare provider in need of quick, reliable information, MedInfoHub is your go-to resource for trusted medical knowledge.")
 # st.title("MedQUAD app")
 
