7fdd964497279ab2feed02dada0c335e998a60ff
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-06-28 03:12:14 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -52,10 +52,18 @@ if my_page == 'About MedInfoHub':
         msg.toast('Initializing...')
         time.sleep(1)
         msg.toast('Ready!', icon = "🥞")
+        status = 'ready'
     if start:
         initializing()
-        with st.container():
-           st.write("This is inside the container")
+        if status =='ready':
+            tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+                data = np.random.randn(10, 1)
+                
+                tab1.subheader("A tab with a chart")
+                tab1.line_chart(data)
+                
+                tab2.subheader("A tab with the data")
+                tab2.write(data)
 
    
 
