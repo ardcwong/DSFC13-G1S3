2c1833874c5e15237aaab32cab3560d1825379a7
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-06-28 17:10:05 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -92,101 +92,193 @@ def compute_similarity(synsets1, synsets2):
                 max_similarity = similarity
     return max_similarity
 
-def process_keyword(filtered_df):
+# def process_keyword(filtered_df):
       
-    column1, column2 = st.columns([1,1])
-    column1.header(keyword)
+#     column1, column2 = st.columns([1,1])
+#     column1.header(keyword)
        
-    keyword_synsets = get_synsets(keyword)
-    focus_area_synsets = {area: get_synsets(area) for area in focus_areas}
+#     keyword_synsets = get_synsets(keyword)
+#     focus_area_synsets = {area: get_synsets(area) for area in focus_areas}
               
-    # Calculate similarities
-    similarities = {}
-    for area, synsets in focus_area_synsets.items():
-        similarity = compute_similarity(keyword_synsets, synsets)
-        similarities[area] = similarity
+#     # Calculate similarities
+#     similarities = {}
+#     for area, synsets in focus_area_synsets.items():
+#         similarity = compute_similarity(keyword_synsets, synsets)
+#         similarities[area] = similarity
     
     
-    # Find the focus area with the highest similarity
+#     # Find the focus area with the highest similarity
+#     best_match_focus_area = max(similarities, key=similarities.get)
+    
+#     column2.header(f"Focus Area - {best_match_focus_area}")
+#     focus_area = best_match_focus_area
+    
+#     if focus_area:
+      
+#         # Filter answers by the selected focus area
+#         filtered_df = df[df['focus_area'].str.lower().str.contains(focus_area, case=False, na=False)]
+        
+#         if not filtered_df.empty:
+#             # Concatenate all answers into a single text
+#             all_answers_text = " ".join(filtered_df['answer'].dropna().tolist())
+#             summary = summarize_answer(all_answers_text)
+#             column1.markdown(summary) 
+            
+#             # Generate word cloud of content of summary of answers
+#             wordcloud = WordCloud(width=800, height=400, background_color='white').generate(all_answers_text)
+            
+#             # Display the word cloud
+#             plt.figure(figsize=(10, 5))
+#             plt.imshow(wordcloud, interpolation='bilinear')
+#             plt.axis('off')
+#             column2.pyplot(plt)
+
+#         else:
+            
+#             column2.write("No matching focus areas found.")
+#     else:
+#             column2.write("No Input keyword.")
+#     return focus_area
+
+# def select_questions(filtered_df):
+#     selected_question = st.selectbox("You may also want to know:", filtered_df['question'].tolist(), index=None)
+#     if selected_question:  
+#         # Display the selected question and its answer
+#         st.write("Selected Question:", selected_question)
+#         selected_answer = filtered_df[filtered_df['question'] == selected_question]['answer'].values[0]
+#         st.write("Answer:", selected_answer)
+#     else:
+#         st.write("No matching questions found.")
+
+# if my_page == 'MedInfoHub':
+    
+#     st.image('data/MIH.png')
+#     col1, col2 = st.columns([1,1])
+#     col1.image('data/art.png')
+#     col2.write("")
+#     col2.write("")
+#     col2.write("MedInfoHub is a comprehensive healthcare app designed to provide accessible medical information to patients and healthcare providers. Leveraging the power of the MedQuAD dataset* and advanced AI, MedInfoHub offers reliable answers to medical questions, supports telemedicine consultations, and enhances public health literacy. Whether you’re a patient seeking to understand your health better or a healthcare provider in need of quick, reliable information, MedInfoHub is your go-to resource for trusted medical knowledge.")
+#     col2.write("*The MedQuAD dataset aggregates content from reputable sources like the National Institutes of Health (NIH), National Library of Medicine (NLM), and other authoritative medical organizations.")
+#     col2.write("Press the 'Activate MedInfoHub' Button to begin exploring MedInfoHub.")
+    
+#     # Displaying the button with custom style
+#     # col_start1, col_start2, col_start3 = st.columns([1,1,1])
+#     on = col2.toggle("Activate MedInfoHub")
+          
+#     # START SESSION     
+#     if not on:
+#         st.session_state['initialized'] = False
+
+#     elif on:
+#           # Check if initializing has been run
+#         if 'initialized' not in st.session_state:
+#             st.session_state['initialized'] = False
+        
+#         if not st.session_state['initialized']:
+#             initializing()
+#             st.session_state['initialized'] = True
+
+#         # ENTER KEYWORD FOR SEMANTIC SIMILARITIES MATCHING WITH FOCUS AREA
+#         st.subheader("Search Keyword Focus Area")
+#         keyword = st.text_input("Enter a keyword to search:")
+#         if keyword:
+#             # Filter questions containing the keyword
+#             filtered_df = df[df['question'].str.contains(keyword, case=False, na=False)]
+            
+#             focus_area = process_keyword(filtered_df)
+#             select_questions(filtered_df)
+
+
+def process_keyword(keyword, df):
+    keyword_synsets = get_synsets(keyword)
+    focus_areas = df['focus_area'].unique()
+    focus_area_synsets = {area: get_synsets(area) for area in focus_areas}
+
+    similarities = {area: compute_similarity(keyword_synsets, synsets) for area, synsets in focus_area_synsets.items()}
     best_match_focus_area = max(similarities, key=similarities.get)
     
-    column2.header(f"Focus Area - {best_match_focus_area}")
-    focus_area = best_match_focus_area
-    
-    if focus_area:
-      
-        # Filter answers by the selected focus area
-        filtered_df = df[df['focus_area'].str.lower().str.contains(focus_area, case=False, na=False)]
-        
-        if not filtered_df.empty:
-            # Concatenate all answers into a single text
-            all_answers_text = " ".join(filtered_df['answer'].dropna().tolist())
-            summary = summarize_answer(all_answers_text)
-            column1.markdown(summary) 
-            
-            # Generate word cloud of content of summary of answers
-            wordcloud = WordCloud(width=800, height=400, background_color='white').generate(all_answers_text)
-            
-            # Display the word cloud
-            plt.figure(figsize=(10, 5))
-            plt.imshow(wordcloud, interpolation='bilinear')
-            plt.axis('off')
-            column2.pyplot(plt)
+    st.session_state['focus_area'] = best_match_focus_area
 
-        else:
-            
-            column2.write("No matching focus areas found.")
+    filtered_df = df[df['focus_area'].str.lower().str.contains(best_match_focus_area, case=False, na=False)]
+    if not filtered_df.empty:
+        all_answers_text = " ".join(filtered_df['answer'].dropna().tolist())
+        summary = summarize_answer(all_answers_text)
+        st.session_state['summary'] = summary
+
+        wordcloud = WordCloud(width=800, height=400, background_color='white').generate(all_answers_text)
+        st.session_state['wordcloud'] = wordcloud
     else:
-            column2.write("No Input keyword.")
-    return focus_area
+        st.session_state['summary'] = "No matching focus areas found."
+        st.session_state['wordcloud'] = None
 
 def select_questions(filtered_df):
     selected_question = st.selectbox("You may also want to know:", filtered_df['question'].tolist(), index=None)
-    if selected_question:  
-        # Display the selected question and its answer
-        st.write("Selected Question:", selected_question)
+    if selected_question:
         selected_answer = filtered_df[filtered_df['question'] == selected_question]['answer'].values[0]
+        st.write("Selected Question:", selected_question)
         st.write("Answer:", selected_answer)
     else:
         st.write("No matching questions found.")
 
-if my_page == 'MedInfoHub':
-    
-    st.image('data/MIH.png')
-    col1, col2 = st.columns([1,1])
-    col1.image('data/art.png')
-    col2.write("")
-    col2.write("")
-    col2.write("MedInfoHub is a comprehensive healthcare app designed to provide accessible medical information to patients and healthcare providers. Leveraging the power of the MedQuAD dataset* and advanced AI, MedInfoHub offers reliable answers to medical questions, supports telemedicine consultations, and enhances public health literacy. Whether you’re a patient seeking to understand your health better or a healthcare provider in need of quick, reliable information, MedInfoHub is your go-to resource for trusted medical knowledge.")
-    col2.write("*The MedQuAD dataset aggregates content from reputable sources like the National Institutes of Health (NIH), National Library of Medicine (NLM), and other authoritative medical organizations.")
-    col2.write("Press the 'Activate MedInfoHub' Button to begin exploring MedInfoHub.")
-    
-    # Displaying the button with custom style
-    # col_start1, col_start2, col_start3 = st.columns([1,1,1])
-    on = col2.toggle("Activate MedInfoHub")
-          
-    # START SESSION     
-    if not on:
-        st.session_state['initialized'] = False
+def initializing():
+    # Dummy function for initialization
+    st.write("Initializing...")
 
-    elif on:
-          # Check if initializing has been run
-        if 'initialized' not in st.session_state:
-            st.session_state['initialized'] = False
+df = pd.DataFrame({
+    'question': ["What is diabetes?", "How to treat hypertension?", "Symptoms of COVID-19?"],
+    'answer': ["Diabetes is a chronic condition.", "Hypertension can be treated with medication.", "Symptoms of COVID-19 include fever and cough."],
+    'focus_area': ["Diabetes", "Hypertension", "COVID-19"]
+})
+
+if 'initialized' not in st.session_state:
+    st.session_state['initialized'] = False
+if 'focus_area' not in st.session_state:
+    st.session_state['focus_area'] = None
+if 'summary' not in st.session_state:
+    st.session_state['summary'] = ""
+if 'wordcloud' not in st.session_state:
+    st.session_state['wordcloud'] = None
+
+if 'on' not in st.session_state:
+    st.session_state['on'] = False
+
+st.image('data/MIH.png')
+col1, col2 = st.columns([1, 1])
+col1.image('data/art.png')
+col2.write("MedInfoHub is a comprehensive healthcare app designed to provide accessible medical information to patients and healthcare providers. Leveraging the power of the MedQuAD dataset* and advanced AI, MedInfoHub offers reliable answers to medical questions, supports telemedicine consultations, and enhances public health literacy. Whether you’re a patient seeking to understand your health better or a healthcare provider in need of quick, reliable information, MedInfoHub is your go-to resource for trusted medical knowledge.")
+col2.write("*The MedQuAD dataset aggregates content from reputable sources like the National Institutes of Health (NIH), National Library of Medicine (NLM), and other authoritative medical organizations.")
+on = st.checkbox("Activate MedInfoHub", value=st.session_state['on'])
+
+if not on:
+    st.session_state['initialized'] = False
+    st.session_state['on'] = False
+else:
+    st.session_state['on'] = True
+    if not st.session_state['initialized']:
+        initializing()
+        st.session_state['initialized'] = True
+
+    st.subheader("Search Keyword Focus Area")
+    keyword = st.text_input("Enter a keyword to search:")
+    if keyword and 'processed_keyword' not in st.session_state:
+        process_keyword(keyword, df)
+        st.session_state['processed_keyword'] = True
+
+    if st.session_state['focus_area']:
+        col1, col2 = st.columns([1, 1])
+        col1.header(keyword)
+        col2.header(f"Focus Area - {st.session_state['focus_area']}")
         
-        if not st.session_state['initialized']:
-            initializing()
-            st.session_state['initialized'] = True
+        col1.markdown(st.session_state['summary'])
+        
+        if st.session_state['wordcloud']:
+            plt.figure(figsize=(10, 5))
+            plt.imshow(st.session_state['wordcloud'], interpolation='bilinear')
+            plt.axis('off')
+            col2.pyplot(plt)
 
-        # ENTER KEYWORD FOR SEMANTIC SIMILARITIES MATCHING WITH FOCUS AREA
-        st.subheader("Search Keyword Focus Area")
-        keyword = st.text_input("Enter a keyword to search:")
-        if keyword:
-            # Filter questions containing the keyword
-            filtered_df = df[df['question'].str.contains(keyword, case=False, na=False)]
-            
-            focus_area = process_keyword(filtered_df)
-            select_questions(filtered_df)
+        filtered_df = df[df['focus_area'].str.lower().str.contains(st.session_state['focus_area'], case=False, na=False)]
+        select_questions(filtered_df)
 
             
             # filtered_df, focus_area = process_keyword()
