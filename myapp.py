22e8f3c0a5cfcf661f55bbd85d274a7c06a901f7
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-06-28 23:44:37 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -25,6 +25,44 @@ client = OpenAI(api_key=api_key)
 SKLLMConfig.set_openai_key(api_key)
 
 st.set_page_config(layout='wide')
+confirmationEdit = Modal("Atención", key= "popUp_edit")
+
+(...) the widgets
+
+submitted = st.button("Enviar")
+
+           if submitted:
+                 confirmationEdit.open()
+        
+           if confirmationEdit.is_open():
+                    with confirmationEdit.container():
+                        st.markdown(""" ### ¿Deseas guardar los cambios? """)
+                        yes = st.button("Sí")
+                        no  = st.button("No")
+
+                        if yes == True:
+                           (...) format data
+                           confirmationEdit.close()
+
+                        if no == True:
+                            confirmationEdit.close()
+# @st.experimental_dialog("Cast your vote")
+# def vote(item):
+#     st.write(f"Why is {item} your favorite?")
+#     reason = st.text_input("Because...")
+#     if st.button("Submit"):
+#         st.session_state.vote = {"item": item, "reason": reason}
+#         st.rerun()
+
+# if "vote" not in st.session_state:
+#     st.write("Vote for your favorite")
+#     if st.button("A"):
+#         vote("A")
+#     if st.button("B"):
+#         vote("B")
+# else:
+#     f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+
 
 st.sidebar.title('MedInfoHub')
 with st.sidebar:
