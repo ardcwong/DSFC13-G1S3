41d9589eb05e0f8d12d6d6710287f9596e2eb518
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-06-28 23:46:25 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -25,27 +25,10 @@ client = OpenAI(api_key=api_key)
 SKLLMConfig.set_openai_key(api_key)
 
 st.set_page_config(layout='wide')
+
 confirmationEdit = Modal("Atención", key= "popUp_edit")
 
-(...) the widgets
 
-submitted = st.button("Enviar")
-
-           if submitted:
-                 confirmationEdit.open()
-        
-           if confirmationEdit.is_open():
-                    with confirmationEdit.container():
-                        st.markdown(""" ### ¿Deseas guardar los cambios? """)
-                        yes = st.button("Sí")
-                        no  = st.button("No")
-
-                        if yes == True:
-                           (...) format data
-                           confirmationEdit.close()
-
-                        if no == True:
-                            confirmationEdit.close()
 # @st.experimental_dialog("Cast your vote")
 # def vote(item):
 #     st.write(f"Why is {item} your favorite?")
