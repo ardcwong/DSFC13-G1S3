016154a61db10992bd17b7536c70b12ac787517e
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-06-28 23:09:31 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -330,6 +330,20 @@ elif on:
 
 
 
+st.title('Telemedicine and Specialty Doctors')
+
+    # Original text with website titles and URLs
+    text = """
+    For telemedicine consultations or to find the nearest specialty doctor near you, you may visit:
+    
+<b>NowServing</b>: NowServing
+<b>Konsulta MD</b>: Konsulta MD
+<b>SeriousMD</b>: SeriousMD
+"""
+
+    # Display formatted text with st.markdown
+    st.markdown(text, unsafe_allow_html=True)
+
 
 
 
