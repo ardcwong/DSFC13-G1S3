17503107c628eb5af7329dbf58d58a87b5d325d3
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-06-29 14:58:50 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -30,7 +30,7 @@ st.set_page_config(layout='wide')
 st.markdown('<p style="font-size: 18px; color: red;"><strong>⚠️ This app is not intended for self-diagnosis or self-treatment. Always consult a qualified healthcare professional for medical advice and diagnosis. ⚠️</strong></p>', unsafe_allow_html=True)
 
 # disable?
-x = "No"
+x = "yes"
 
 # @st.experimental_dialog("Cast your vote")
 # def vote(item):
